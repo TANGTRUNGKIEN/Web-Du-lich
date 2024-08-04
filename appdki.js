1fefb455f3889b7bfb2905756032a0acccdb1026
Le Dung <ledung12022002@gmail.com>
2024-08-04 16:08:58 +0700

    update

--- appdki.js
+++ appdki.js
@@ -1,16 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("registration-form");
-    const message = document.getElementById("message");
-  
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const formData = new FormData(form);
-        const data = {};
-        formData.forEach(function(value, key) {
-            data[key] = value;
-        });
-  
-        message.textContent = "Đăng ký thành công!";
-        form.reset();
-    });
-  });
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const auth = getAuth();
+
+const signupForm = document.getElementById('registration-form');
+
+signupForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    window.location.href = '/index-tk.html'
+  } catch (error) {
+    alert(`Đăng ký thất bại. Lỗi: ${ error.message }`);
+  }
+});
