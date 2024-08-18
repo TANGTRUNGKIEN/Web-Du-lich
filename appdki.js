e56da6829304adf5c21943e408868cdbc9e9c3b8
Le Dung <ledung12022002@gmail.com>
2024-08-18 15:56:53 +0700

    h

--- appdki.js
+++ appdki.js
@@ -1,5 +1,8 @@
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { app } from "/js/config.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const db = getFirestore(app);
 const auth = getAuth();
 
 const signupForm = document.getElementById('registration-form');
@@ -9,12 +12,26 @@ signupForm.addEventListener('submit', async (e) => {
 
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
+  const ngaysinh = document.getElementById('ngaysinh').value;
+  const diachi = document.getElementById('diachi').value;
+  const hoten = document.getElementById('hoten').value;
 
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
-    window.location.href = '/index-tk.html'
+
+    const data_user = {
+      hoten: hoten,
+      ngaysinh: ngaysinh,
+      diachi: diachi,
+    };
+
+    const userDocRef = doc(db, "users", user.uid);
+    await setDoc(userDocRef, data_user);
+
+    alert(`Đăng ký thành công! User ID: ${user.uid}`);
+    window.location.href = '/index-gt.html';
   } catch (error) {
-    alert(`Đăng ký thất bại. Lỗi: ${ error.message }`);
+    alert(`Đăng ký thất bại. Lỗi: ${error.message}`);
   }
 });
